68c3aa0df5c393f1cc2aa9106802b74a5079abe8
Sebastian Weiß <sw714201@fh-muenster.de>
2021-01-21 01:24:16 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -l
-alias gbdkcc='gcc -I /usr/lib/gbdk/include/'
+alias gbdkcc='/usr/lib/gbdk/lcc -Wa-l -Wl-j -DUSE_SFR_FOR_REG -I /usr/lib/gbdk/include/'
 
 echo $PATH
 PATH=${PATH}:/usr/lib/gbdk/include/
@@ -9,4 +9,5 @@ echo "Chosen directory: $1"
 cd $1
 
 # Compile main.c
-gbdkcc -o myapp main.c
+gbdkcc -c -o game.o main.c
+gbdkcc -o game.gb game.o
