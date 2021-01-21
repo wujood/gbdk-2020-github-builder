349f524c142af50e80e0ec6aa9892d8c3738c00f
Sebastian Weiß <sw714201@fh-muenster.de>
2021-01-21 01:14:02 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,13 @@
 #!/bin/sh -l
-alias gbdkcc='gcc -I /usr/lib/gdbk/include/**'
+alias gbdkcc='gcc -I /usr/lib/gdbk/include/'
 
 ls /usr/lib/gbdk/
 echo "-----------"
 ls /usr/lib/gbdk/include/
 echo "-----------"
 
+echo $PATH
+PATH=${PATH}:/usr/lib/gdbk/include/
 echo "Chosen directory: $1"
 cd $1
 
